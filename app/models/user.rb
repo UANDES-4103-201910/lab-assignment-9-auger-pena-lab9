ec898a6fcd33fd730f80e1a64f2f5c0abbe1c8af
root <root@progra-bajo-nivel.gamerlive.cl>
2019-05-23 15:08:24 -0400

    pending cancancan abilities checking

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,12 @@ class User < ApplicationRecord
 	validates :phone, length: {minimum: 9, maximum: 12}, allow_blank: true
   	validates :password, format: {with: /\A[a-zA-Z0-9\.]{8,12}\z/ , message: "assword must be between 8 to 12 alphanumeric characters"}
 
+  def admin?
+        has_role?(:admin)
+  end 
+  def user?
+        has_role?(:user)
+  end 
 
 
 end
